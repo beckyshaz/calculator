3d99a785975e6a87e601cd63ce6c039ebe5346e9
beckyshaz <sharonsakah8@gmail.com>
2025-05-26 13:24:39 +0300

    created functions that perform basic math operation like division, multiplication , addition and subtraction for the basic calculator app

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("hello world");
+
+
+function add(a, b) {
+    return a + b;
+}  
+let a = 5;
+let b = 2;
+console.log(add(5, 2));
+
+
+function subtract(a, b) {
+    return a - b;
+}  
+
+let d = 2;
+let e = 5;
+console.log(subtract(5, 2));
+
+
+function multiply(a, b) {
+    return a * b;
+}  
+
+let f = 5;
+let g = 2;
+console.log(multiply(5, 2));
+
+
+
+
+function devide(a, b) {
+    return a / b;
+}  
+
+
+let x = 5;
+let y = 2;
+console.log(devide(5, 2));
